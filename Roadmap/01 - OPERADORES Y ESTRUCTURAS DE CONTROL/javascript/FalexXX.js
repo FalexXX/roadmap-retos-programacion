e377f7a95bed06749503f1d99068616d91a5243c
fabrizio <fabri.lveintimilla@gmail.com>
2025-01-23 18:13:22 -0500

    Avance del ejercicio 01

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/FalexXX.js
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/FalexXX.js
@@ -1,1 +1,73 @@
--
+/*
+ Operadores
+*/
+
+
+//Operadores Aritméticos
+suma = 45 + 6 
+console.log ("Suma: 45 + 6 = ", suma)
+
+resta = 45 - 6 
+console.log ("Resta: 45 - 6 = ", resta)
+
+multiplicacion = 45 * 6 
+console.log ("multiplicacion: 45 * 6 = ", multiplicacion)
+
+division = 45 / 6 
+console.log ("division: 45 / 6 = ", division)
+
+residuo = 45 % 6 
+console.log ("Residuo: 45 % 6 = ", residuo)
+
+exponente = 45 ** 6 
+console.log ("Exponente: 45 ** 6 = ", exponente)
+
+
+//Operadores de asignación
+
+// x = y 
+// x += y
+// x -= y
+// x *= y
+// x /= y 
+// x %= y
+// x **= y 
+
+//Operadores de comparación
+
+// x == y
+// x != y
+// x === y 
+// x !== y
+// x > y
+// x >= y
+// x < y
+// x <= y
+
+//Operadores logicos
+
+// if(x & y)
+
+// if (x || y)
+
+
+//Operadores de concatenación
+
+console.log ("Hola" + " como estas?")
+
+//Operador Condicional 
+
+var n1 = 4
+var n2 = 5
+
+n1 > n2 ? console.log("No es menor") : console.log("Es menor");
+
+//Operador coma
+
+var cad  = [1,2,3,4,5,6,7,8]
+var obj = {nombre: "Carlos", apellido: "Vaca", edad: 45}
+
+
+for  (var i= 0, j=10; i <= j; i++, j--){
+    console.log ("a ["+ i+"] ["+ j +"]")
+}
