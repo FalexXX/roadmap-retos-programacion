86fb51696e27dcf7a16036c145c6c41eb7b4c603
fabrizio <fabri.lveintimilla@gmail.com>
2025-01-22 23:18:01 -0500

    Solución del ejercicio #00

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/FalexXX.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/FalexXX.js
@@ -1,1 +1,27 @@
-print("HOLA MUNDO");
+
+// EMPEZANDO A ENTENDER LA LOGÍCA DE PROGRAMACIÓN DESDE 0
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+/* 
+ Comentario de 
+ varias 
+ lineas 
+ de texto
+*/
+
+var text = "Esto es una variable de tipo String" 
+const Constante = "Esto es una constante de tipo String"
+
+// Datos primitivos
+    var NumberType = 77
+    var StringType ="Juan Perez"
+    var BooleanType = false
+    var bool = true
+    var UndefinedType = undefined
+    var NullType = null
+    var SymbolType = Symbol('desc')
+    var ObjectType = {color : 'morado', nombre : 'tulipan', costo : 2}
+
+//Impresión 
+    console.log( "¡Hola, JavaScript!")
